9db7b35f5b95ec212d7c2987561308db471408fd
ValentinoChiappanni <valentino.chiappanni@estudiantes.unahur.edu.ar>
2023-08-31 20:04:38 -0300

    Nueva ext y prueba3

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
-import {
-  BrowserRouter,
-  Route,
-  Routes
-} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import AdminWeb from './components/AdminWeb/AdminWeb';
 import ScreenClient from './components/ScreenClient/ScreenClient';
+import AdminWeb from './components/AdminWeb/AdminWeb';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={
-          <div>
-            <h1>App</h1>
-          </div>
-        }/>
-        <Route path='/screen' element={<ScreenClient/>}/>
-        <Route path='/admin' element={<AdminWeb/>}/>
+        <Route
+          path="/"
+          element={
+            <div>
+              <h1>App</h1>
+            </div>
+          }
+        />
+        <Route path="/screen" element={<ScreenClient />} />
+        <Route path="/admin" element={<AdminWeb />} />
       </Routes>
     </BrowserRouter>
   );
