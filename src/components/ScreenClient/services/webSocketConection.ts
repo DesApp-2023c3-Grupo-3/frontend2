49096f0fdc4ddabaaa0e20532e3e7451eaf7bb10
ValentinoChiappanni <valentino.chiappanni@estudiantes.unahur.edu.ar>
2023-09-22 21:48:57 -0300

    Errrores

--- src/components/ScreenClient/services/webSocketConection.ts
+++ src/components/ScreenClient/services/webSocketConection.ts
@@ -1,30 +1,34 @@
 const HOST = 'localhost';
 const PORT = 1234;
 
-export const initializeSocketConnection = async (onMessageAction: any): Promise<WebSocket> => {
-    try {
-      const wsUrl = `ws://${HOST}:${PORT}`;
-      const ws = new WebSocket(wsUrl)
-  
-      ws.addEventListener('open', () => {
-        console.log(`WebSocket Connected ${wsUrl}`);
-        ws.send(JSON.stringify({
+export const initializeSocketConnection = async (
+  onMessageAction: any,
+): Promise<WebSocket> => {
+  try {
+    const wsUrl = `ws://${HOST}:${PORT}`;
+    const ws = new WebSocket(wsUrl);
+
+    ws.addEventListener('open', () => {
+      console.log(`WebSocket Connected ${wsUrl}`);
+      ws.send(
+        JSON.stringify({
           sectorId: 1,
-          message: 'Hi! This is a client'
-        }));
-      });
-      
-      ws.addEventListener('message', (message) => {
-        onMessageAction(JSON.parse(message.data));
-      });
-  
-      ws.addEventListener('error', (error) => {
-        console.log(`ERROR: ${error}`)
-      });
-  
-      return ws;
-    } catch (error) {
-      console.error('NATS Connection ERROR: ', error);
-      throw error;
-    }
+          message: 'Hi! This is a client',
+        }),
+      );
+    });
+
+    ws.addEventListener('message', (message) => {
+      onMessageAction(JSON.parse(message.data));
+    });
+
+    ws.addEventListener('error', (error) => {
+      console.log(`ERROR: ${error}`);
+    });
+
+    return ws;
+  } catch (error) {
+    console.error('NATS Connection ERROR: ', error);
+    throw error;
   }
+};
--- src/components/ScreenClient/store/socketStore.ts
+++ src/components/ScreenClient/store/socketStore.ts
@@ -1,61 +1,60 @@
-import { create } from "zustand";
-import { messages } from "../mocks/imagenes";
-import { filterMessages } from "../utils/arrays";
-
+import { create } from 'zustand';
+import { messages } from '../mocks/imagenes';
+import { filterMessages } from '../utils/arrays';
 
 export interface Message {
-    topic: string,
-    id: number,
-    data: any
+  topic: string;
+  id: number;
+  data: any;
 }
 
 export interface DataAdvertesing {
-    advertisingTypeId: number
-    id: number
-    payload: string
-    title: string
+  advertisingTypeId: number;
+  id: number;
+  payload: string;
+  title: string;
 }
 
 export interface DataCourse {
-        id: number,
-        subject: string,
-        title: string,
-        classroom: string,
-        schedule: string
+  id: number;
+  subject: string;
+  title: string;
+  classroom: string;
+  schedule: string;
 }
 
 export type Store = {
-    messages: Message [],
-    getAdvertisingMessages: () => any [],
-    getCoursesMessages: () => any [],
-    addMessage: (message:Message) => void,
-    deleteMessage: (id: number) => void,
-}
+  messages: Message[];
+  getAdvertisingMessages: () => any[];
+  getCoursesMessages: () => any[];
+  addMessage: (message: Message) => void;
+  deleteMessage: (id: number) => void;
+};
 
-const INITIAL_STATE : Message [] = messages
+const INITIAL_STATE: Message[] = messages;
 
 const TYPE_MESSAGES = {
-    advertising: "advertising",
-    courses: "course",
-    connection: "connection"
-}
+  advertising: 'advertising',
+  courses: 'course',
+  connection: 'connection',
+};
 
-export const useSocketStore = create<Store>() ((set, get) => ({
-    messages: INITIAL_STATE,
-    getAdvertisingMessages: () => filterMessages(get().messages, TYPE_MESSAGES.advertising),
-    getCoursesMessages: () => filterMessages(get().messages, TYPE_MESSAGES.courses),
+export const useSocketStore = create<Store>()((set, get) => ({
+  messages: INITIAL_STATE,
+  getAdvertisingMessages: () =>
+    filterMessages(get().messages, TYPE_MESSAGES.advertising),
+  getCoursesMessages: () =>
+    filterMessages(get().messages, TYPE_MESSAGES.courses),
 
-    addMessage: (message) => { 
-        set(state => ({ 
-            messages: [
-                ...state.messages, message
-            ] 
-        })) 
-    },
+  addMessage: (message) => {
+    set((state) => ({
+      messages: [...state.messages, message],
+    }));
+  },
 
-    deleteMessage: (id) => {
-        set(state => ({
-            messages: state.messages.filter(message => message.id !== id)
-        }))
-    }
-})) 
+  deleteMessage: (id) => {
+    set((state) => ({
+      messages: state.messages.filter((message) => message.id !== id),
+    }));
+  },
+}));
