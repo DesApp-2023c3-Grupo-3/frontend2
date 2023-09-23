8337083c3f971b434acf12a95900b562372d8f63
Cristian Juarez <cristianjuarezdev@gmail.com>
2023-09-22 22:21:26 -0300

    Added Pantallas

--- src/components/AdminWeb/AdminWeb.tsx
+++ src/components/AdminWeb/AdminWeb.tsx
@@ -1,19 +1,21 @@
-import './AdminWeb.sass'
-import { Route, Routes } from 'react-router-dom'
-import Navbar from './Navbar'
-import Avisos from './Avisos/Avisos'
-import Usuarios from './Usuarios/Usuarios'
-import Comisiones from './Comisiones/Comisiones'
+import './AdminWeb.sass';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './Navbar';
+import Avisos from './Avisos/Avisos';
+import Usuarios from './Usuarios/Usuarios';
+import Comisiones from './Comisiones/Comisiones';
+import Pantallas from './Pantallas/Pantallas';
 
 function AdminWeb() {
   return (
-    <section className='h-screen flex flex-row'>
-      <Navbar/>
+    <section className="h-screen flex flex-row">
+      <Navbar />
       <h1>Admin Web</h1>
       <Routes>
-        <Route path="/avisos" element={<Avisos/>} />
-        <Route path="/comisiones" element={<Comisiones/>} />
-        <Route path="/usuarios" element={<Usuarios/>} />
+        <Route path="/advertising" element={<Avisos />} />
+        <Route path="/comission" element={<Comisiones />} />
+        <Route path="/screen" element={<Pantallas />} />
+        <Route path="/user" element={<Usuarios />} />
       </Routes>
     </section>
   );
--- src/components/AdminWeb/Navbar.tsx
+++ src/components/AdminWeb/Navbar.tsx
@@ -1,27 +1,47 @@
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import adminPicture from './assets/admin.png';
 
 function Navbar() {
-  const [navDeployed, setNavDeployed] = useState(false)
+  const [navDeployed, setNavDeployed] = useState(false);
 
   const switchNavbar = () => {
-    setNavDeployed(!navDeployed)
-  }
+    setNavDeployed(!navDeployed);
+  };
 
   return (
-    <nav className={"fixed md:relative h-screen px-12 py-8 flex flex-col gap-2 " + (navDeployed?'translate-x-[-100%]':'translate-x-[0%]')}>
-      <button className={'md:hidden absolute cursor-pointer right-[-2rem]'} onClick={switchNavbar}>X</button>
+    <nav
+      className={
+        'fixed md:relative h-screen px-12 py-8 flex flex-col gap-2 ' +
+        (navDeployed ? 'translate-x-[-100%]' : 'translate-x-[0%]')
+      }
+    >
+      <button
+        className={'md:hidden absolute cursor-pointer right-[-2rem]'}
+        onClick={switchNavbar}
+      >
+        X
+      </button>
       <div className="flex items-center gap-4 bg-white border-white rounded-[10rem] pr-12 mb-16">
-        <span className="aspect-square h-16 overflow-hidden rounded-[10rem]"><img src="https://avatars.githubusercontent.com/u/84170516?v=4" alt="User avatar" /></span>
+        <span className="aspect-square h-16 overflow-hidden rounded-[10rem] p-[0.25rem]">
+          <img src={adminPicture} alt="User avatar" />
+        </span>
         <h3 className="font-semibold itim">Administrador</h3>
       </div>
-      <NavLink to='/admin/avisos'>Avisos</NavLink>
-      <NavLink to='/admin/comisiones'>Comisiones</NavLink>
-      <NavLink to='/admin/usuarios'>Administrar usuarios</NavLink>
-      <a className='sign-out mt-auto' href='/'>Cerrar sesion</a>
-      <img className='mx-auto aspect-square h-20 mt-6' src="https://unahur.edu.ar/wp-content/uploads/2021/03/UNAHUR-1.png" alt="" />
+      <NavLink to="/admin/advertising">Avisos</NavLink>
+      <NavLink to="/admin/comission">Comisiones</NavLink>
+      <NavLink to="/admin/screen">Pantallas</NavLink>
+      <NavLink to="/admin/user">Administrar usuarios</NavLink>
+      <a className="sign-out mt-auto" href="/">
+        Cerrar sesion
+      </a>
+      <img
+        className="mx-auto aspect-square h-20 mt-6"
+        src="https://unahur.edu.ar/wp-content/uploads/2021/03/UNAHUR-1.png"
+        alt=""
+      />
     </nav>
-  )
+  );
 }
 
 export default Navbar;
