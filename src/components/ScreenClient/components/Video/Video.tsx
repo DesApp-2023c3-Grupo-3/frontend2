a1c469dfc2bfe455e554dea4182f3651320cb7a3
lucianogaregnani <luciano.garegnani@estudiantes.unahur.edu.ar>
2023-10-06 02:19:57 -0300

    nueva version de la cartelera con video

--- src/components/ScreenClient/components/Video/Video.tsx
+++ src/components/ScreenClient/components/Video/Video.tsx
@@ -3,7 +3,6 @@ import { useSocketStore } from '../../store/socketStore';
 import AdvertisingVideo from '../Advertising/VideoAdvertising';
 import Footer from '../Course/Footer';
 import Header from '../Course/Header';
-import Dots from '../Dots';
 
 export default function Video() {
   const advertisingMessages = useSocketStore((state) =>
@@ -13,7 +12,7 @@ export default function Video() {
     (message) => message.advertisingTypeId === 2,
   );
 
-  const { selectedItem, selectedIndex, changeSelectedItem } = useCarousel(
+  const { selectedItem, changeSelectedItem } = useCarousel(
     advertisingVideos,
     0,
   );
@@ -21,25 +20,15 @@ export default function Video() {
   return (
     <section className="h-screen col-start-4 col-end-13 bg-white flex flex-col justify-between">
       <Header />
-      <div className="h-full w-full p-2 mx-auto">
-        <div className="h-full shadow-[0px_0px_10px_rgba(0,0,0,0.15)] rounded-xl overflow-hidden">
-          <div className="w-full h-[8%] bg-[#74B235] text-white text-[3.5vh]">
-            <span className="ml-10">Becas Progresar</span>
-          </div>
-          <div className="h-[92%] w-full p-4">
-            <AdvertisingVideo
-              payload={selectedItem.payload}
-              changeSelectedItem={changeSelectedItem}
-              sx="h-full w-full"
-            />
-          </div>
-        </div>
-      </div>
-      <Dots
-        selectedIndex={selectedIndex}
-        items={advertisingVideos}
-        sx="items-center justify-center"
-      />
+      <article className="h-full w-full p-3 mx-auto">
+        <main className="h-full w-full shadow-[0px_0px_10px_rgba(0,0,0,0.15)] rounded-xl overflow-hidden">
+          <AdvertisingVideo
+            payload={selectedItem.payload}
+            changeSelectedItem={changeSelectedItem}
+            sx="h-full w-full"
+          />
+        </main>
+      </article>
       <Footer />
     </section>
   );
