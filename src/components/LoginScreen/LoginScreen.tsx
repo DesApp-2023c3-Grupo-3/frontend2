e641c4625137a65f985f3a618fe0b1c081b748e9
Cristian Juarez <cristianjuarezdev@gmail.com>
2023-10-06 17:35:57 -0300

    Login screen maquetado

---
+++ src/components/LoginScreen/LoginScreen.tsx
@@ -0,0 +1,48 @@
+import './LoginScreen.sass';
+import unahurLogo from './assets/unahur.png';
+
+function LoginScreen() {
+  return (
+    <section className="flex flex-row login">
+      <article className="hidden md:flex flex-col justify-center h-screen px-32">
+        <img src={unahurLogo} alt="UNAHUR Logo" />
+        <div className="flex flex-col text-left mt-16">
+          <span className="font-bold text-3xl w-[20ch]">
+            Sistema Universitario de Gestión de Carteleras
+          </span>
+          <div className="line mt-4 w-[34ch]" />
+        </div>
+      </article>
+      <article className="flex flex-col justify-center items-center md:ml-auto bg-[#609E2F] w-[100%] md:w-fit h-screen md:px-36 text-white">
+        <div className="flex flex-col text-center items-center md:hidden">
+          <span className="opacity-[0.9] text-xl w-[20ch]">
+            Sistema Universitario de Gestión de Carteleras
+          </span>
+          <div className="line thin mt-4 w-[21ch]" />
+        </div>
+        <h3 className="text-3xl font-bold tracking-wide mt-20">¡Bienvenido!</h3>
+        <span className="text-sm mt-8 opacity-80">
+          Inicia sesión con tus datos
+        </span>
+        <form action="" className="flex flex-col mt-4 gap-4 w-max">
+          <input type="text" placeholder="Nombre de usuario" name="username" />
+          <input type="password" placeholder="Contraseña" name="password" />
+          <span className="text-sm text-center mt-4 opacity-80">
+            O ingrese el código de pantalla
+          </span>
+          <input type="text" placeholder="ID de pantalla" name="screen-id" />
+          <button type="submit" className="mt-4 font-bold">
+            Ingresar
+          </button>
+        </form>
+        <img
+          src={unahurLogo}
+          alt="UNAHUR Logo"
+          className="brightness-0 invert-[1] mt-10 w-36 md:hidden"
+        />
+      </article>
+    </section>
+  );
+}
+
+export default LoginScreen;
