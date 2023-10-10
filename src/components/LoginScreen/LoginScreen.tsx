c47b9d4254cb228341d5034ac64e5a6913b170b9
Cristian Juarez <cristianjuarezdev@gmail.com>
2023-10-10 18:56:45 -0300

    Validaciones

--- src/components/LoginScreen/LoginScreen.tsx
+++ src/components/LoginScreen/LoginScreen.tsx
@@ -1,26 +1,46 @@
 import './LoginScreen.sass';
 import unahurLogo from './assets/unahur.png';
-import { FormEvent, useState } from 'react';
+import { FormEvent, useRef, useState } from 'react';
 import { useNavigate } from "react-router-dom";
 
 function LoginScreen({setScreenId}: {setScreenId: any}) {
   const navigate = useNavigate();
-  const [newScreenId, setNewScreenId] = useState(1);
+  const usernameRef = useRef<HTMLInputElement>(null)
+  const passwordRef = useRef<HTMLInputElement>(null)
+  const screenIdRef = useRef<HTMLInputElement>(null)
+  const [invalidNotice, setInvalidNotice] = useState('')
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault()
-    
-    // TODO Validacion de datos
+    // TODO Validacion screen
+    if(invalidScreenId()) { setInvalidNotice('El código de pantalla es invalido.'); return }
+    if(screenIdRef.current?.value.trim() !== '') { navigateToScreen(); return }
+
+    // Validacion login
+    if(invalidUsername()) { setInvalidNotice('No se ha encontrado el usuario.'); return }
+    if(invalidPassword()) { setInvalidNotice('La contraseña es incorrecta.'); return }
+
     // TODO Login para ingresar a AdminWeb
-    //
-    // Intento de conexion ID pantalla
-    // TODO Vista de error
-    setScreenId(newScreenId)
-    navigate('/screen')
+    navigate('/admin')
   }
 
-  const onChangeScreenId = (e: React.FormEvent<HTMLInputElement>) => {
-    setNewScreenId(parseInt(e.currentTarget.value))
+  const invalidUsername = () => {
+    return (usernameRef.current?.value.trim() === '')
+  }
+
+  const invalidPassword = () => {
+    return (passwordRef.current?.value.trim() === '')
+  }
+
+  const invalidScreenId = () => {
+    return (passwordRef.current?.value.trim() === '1')
+  }
+
+  const navigateToScreen = () => {
+    // Intento de conexion ID pantalla
+    // TODO Vista de error
+    setScreenId(screenIdRef.current?.value || 1)
+    navigate('/screen')
   }
 
   return (
@@ -46,13 +66,14 @@ function LoginScreen({setScreenId}: {setScreenId: any}) {
           Inicia sesión con tus datos
         </span>
         <form onSubmit={(e) => handleSubmit(e)} className="flex flex-col mt-4 gap-4 w-max">
-          <input type="text" placeholder="Nombre de usuario" name="username" />
-          <input type="password" placeholder="Contraseña" name="password" />
+          <input type="text" placeholder="Nombre de usuario" name="username" ref={usernameRef}/>
+          <input type="password" placeholder="Contraseña" name="password" ref={passwordRef}/>
           <span className="text-sm text-center mt-4 opacity-80">
             O ingrese el código de pantalla
           </span>
-          <input type="text" placeholder="ID de pantalla" name="screen-id"  onChange={(e) => onChangeScreenId(e)}/>
+          <input type="number" placeholder="ID de pantalla" name="screen-id" ref={screenIdRef}/>
           <button type="submit" className="mt-4 font-bold">Ingresar</button>
+          <span>{invalidNotice}</span>
         </form>
         <img
           src={unahurLogo}
