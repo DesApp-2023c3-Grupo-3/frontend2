bc844a548c15ca664b1aad386faad3b830f588a6
tomas0011 <tomasbenjamin117@gmail.com>
2023-10-19 15:56:59 -0300

    base url

--- src/components/ScreenClient/services/fetchAdvertisings.ts
+++ src/components/ScreenClient/services/fetchAdvertisings.ts
@@ -1,5 +1,7 @@
+const BASE_URL = process.env.REACT_APP_API || "http://[::1]:3001/"
+
 export const fetchAdvertisings = async (screenId:number) => {
-    const request = await fetch(`http://localhost:3001/advertising/screen/${screenId}`)
+    const request = await fetch(`${BASE_URL}/advertising/screen/${screenId}`)
     const advertisings = await request.json()
   
     return advertisings
--- src/components/ScreenClient/services/fetchCourses.ts
+++ src/components/ScreenClient/services/fetchCourses.ts
@@ -1,5 +1,7 @@
+const BASE_URL = process.env.REACT_APP_API || "http://[::1]:3001/"
+
 export const fetchCourses = async (sectorId:number) => {
-    const request = await fetch(`http://localhost:3001/course/sector/${sectorId}`)
+    const request = await fetch(`${BASE_URL}/course/sector/${sectorId}`)
     const Courses = await request.json()
   
     return Courses
