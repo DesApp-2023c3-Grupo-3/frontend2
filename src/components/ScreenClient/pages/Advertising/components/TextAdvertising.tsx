8ec8fc55db237c1373490218ce73452df73434c8
tomas0011 <tomasbenjamin117@gmail.com>
2023-10-19 16:54:30 -0300

    add flex-col

--- src/components/ScreenClient/pages/Advertising/components/TextAdvertising.tsx
+++ src/components/ScreenClient/pages/Advertising/components/TextAdvertising.tsx
@@ -2,7 +2,7 @@ export default function AdvertisingText({ payload }: { payload: string }) {
   return (
     <article
       dangerouslySetInnerHTML={{ __html: payload }}
-      className="flex justify-center items-center bg-[#E3E3E3] h-[85%] w-[85%] rounded-[10%]"
+      className="flex flex-col justify-center items-center bg-[#E3E3E3] h-[85%] w-[85%] rounded-[10%]"
     />
   );
 }
