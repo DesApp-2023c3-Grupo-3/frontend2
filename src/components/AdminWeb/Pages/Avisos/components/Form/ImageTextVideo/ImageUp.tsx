9df43cf81a7f99ae066a71e11beb928846546f7e
EnzoAlarcon <enzoalarcon01@gmail.com>
2023-11-09 14:04:49 -0300

    fix subir imagen

--- src/components/AdminWeb/Pages/Avisos/components/Form/ImageTextVideo/ImageUp.tsx
+++ src/components/AdminWeb/Pages/Avisos/components/Form/ImageTextVideo/ImageUp.tsx
@@ -80,29 +80,31 @@ export default ImageUp;
 const upImage = (handleImageUpload: any) => {
   return (
     <>
-      <input
-        type="file"
-        id="image-upload"
-        accept="image/*"
-        style={{ display: 'none' }}
-        onChange={handleImageUpload}
-      />
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="36"
-        height="45"
-        viewBox="0 0 36 45"
-        fill="none"
-        className="cursor-pointer"
-      >
-        <path
-          d="M34 12.5V40.5C34 41.0304 33.7893 41.5391 33.4142 41.9142C33.0391 42.2893 32.5304 42.5 32 42.5H4C3.46957 42.5 2.96086 42.2893 2.58579 41.9142C2.21071 41.5391 2 41.0304 2 40.5V4.5C2 3.96957 2.21071 3.46086 2.58579 3.08579C2.96086 2.71071 3.46957 2.5 4 2.5H24M34 12.5H24V2.5M34 12.5L24 2.5M18 19.5V33.5M11 26.5H25"
-          stroke="#545454"
-          strokeWidth="4"
-          strokeLinecap="round"
-          strokeLinejoin="round"
+      <label>
+        <input
+          type="file"
+          id="image-upload"
+          accept="image/*"
+          style={{ display: 'none' }}
+          onChange={handleImageUpload}
         />
-      </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="36"
+          height="45"
+          viewBox="0 0 36 45"
+          fill="none"
+          className="cursor-pointer"
+        >
+          <path
+            d="M34 12.5V40.5C34 41.0304 33.7893 41.5391 33.4142 41.9142C33.0391 42.2893 32.5304 42.5 32 42.5H4C3.46957 42.5 2.96086 42.2893 2.58579 41.9142C2.21071 41.5391 2 41.0304 2 40.5V4.5C2 3.96957 2.21071 3.46086 2.58579 3.08579C2.96086 2.71071 3.46957 2.5 4 2.5H24M34 12.5H24V2.5M34 12.5L24 2.5M18 19.5V33.5M11 26.5H25"
+            stroke="#545454"
+            strokeWidth="4"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      </label>
     </>
   );
 };
