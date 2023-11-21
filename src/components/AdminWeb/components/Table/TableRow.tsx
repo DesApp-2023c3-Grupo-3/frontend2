504c2c3e1813962d2ef0243f189c670e1786dfaf
Enzo <enzoalarcon01@gmail.com>
2023-11-21 12:02:55 -0300

    ellipsis en los nombres de los avisos en la tabla

--- src/components/AdminWeb/components/Table/TableRow.tsx
+++ src/components/AdminWeb/components/Table/TableRow.tsx
@@ -24,7 +24,11 @@ function TableRow({ item, index, columns, onRowClick, rowRef }: TableRowProps) {
     >
       {Array.from(columns.keys()).map((columnName) => {
         return (
-          <td key={columnName} id={columnName} className="py-2 px-6">
+          <td
+            key={columnName}
+            id={columnName}
+            className="py-2 px-6 text-ellipsis max-w-[220px] overflow-hidden"
+          >
             {columns.get(columnName)?.call(item, item) || '-'}
           </td>
         );
