fe9ba3d4e85716fba260edcaa289cf96763605cf
lucianogaregnani <luciano.garegnani@estudiantes.unahur.edu.ar>
2023-11-23 20:43:23 -0300

    orden de pantallas por id

--- src/components/AdminWeb/Pages/Pantallas/components/ScreenMain/ScreenMain.tsx
+++ src/components/AdminWeb/Pages/Pantallas/components/ScreenMain/ScreenMain.tsx
@@ -10,11 +10,15 @@ function ScreenMain() {
     (screen) => screen.sectorTitle === sector || sector === 'Todos',
   );
 
+  const ordScreensBySector = filteredScreens.sort(
+    (screen1, screen2) => screen1.id - screen2.id,
+  );
+
   return (
     <main className="h-full">
       <section className="flex justify-center flex-wrap gap-4">
         {filteredScreens.length > 0 ? (
-          filteredScreens.map((screen) => (
+          ordScreensBySector.map((screen) => (
             <ScreenCard
               key={screen.id}
               id={screen.id}
