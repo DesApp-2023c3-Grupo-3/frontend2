8bd7c8e67e3eeb15da14930706d8f59bc8596e89
Cristian Juarez <cristianjuarezdev@gmail.com>
2023-11-23 20:00:17 -0300

    Navbar close on navigation & Navbar burger button

--- src/components/AdminWeb/Navbar.tsx
+++ src/components/AdminWeb/Navbar.tsx
@@ -6,6 +6,7 @@ function Navbar() {
   const [navDeployed, setNavDeployed] = useState(false);
 
   const switchNavbar = () => {
+    if (!/Mobi|Android/i.test(navigator.userAgent)) return;
     setNavDeployed(!navDeployed);
   };
 
@@ -20,7 +21,21 @@ function Navbar() {
         className={'md:hidden absolute cursor-pointer right-[-2rem]'}
         onClick={switchNavbar}
       >
-        X
+        <svg
+          width="22"
+          height="16"
+          viewBox="0 0 22 16"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M2 2H20M2 8H20M2 14H20"
+            stroke="black"
+            strokeWidth="2.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
       </button>
       <div className="flex items-center gap-4 bg-white border-white rounded-[10rem] pr-12 mb-16">
         <span className="aspect-square h-16 overflow-hidden rounded-[10rem] p-[0.25rem]">
@@ -28,10 +43,17 @@ function Navbar() {
         </span>
         <h3 className="font-semibold itim">Administrador</h3>
       </div>
-      <NavLink to="/admin/advertising">Avisos</NavLink>
-      <NavLink to="/admin/comission">Comisiones</NavLink>
-      <NavLink to="/admin/screen">Pantallas</NavLink>
-      <NavLink to="/admin/user">Administrar usuarios</NavLink>
+      <div
+        onClick={switchNavbar}
+        onKeyDown={switchNavbar}
+        className="relative flex flex-col"
+        role="presentation"
+      >
+        <NavLink to="/admin/advertising">Avisos</NavLink>
+        <NavLink to="/admin/comission">Comisiones</NavLink>
+        <NavLink to="/admin/screen">Pantallas</NavLink>
+        <NavLink to="/admin/user">Administrar usuarios</NavLink>
+      </div>
       <NavLink to="/" className="sign-out mt-auto">
         Cerrar sesion
       </NavLink>
