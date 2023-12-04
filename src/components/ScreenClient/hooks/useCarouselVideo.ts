5d0706d63b976fa680901bfa97b42b7e48af0035
lucianogaregnani <luciano.garegnani@estudiantes.unahur.edu.ar>
2023-12-04 00:22:51 -0300

    fixed

--- src/components/ScreenClient/hooks/useCarouselVideo.ts
+++ src/components/ScreenClient/hooks/useCarouselVideo.ts
@@ -4,13 +4,17 @@ export function useCarouselVideo(items: any[]) {
   const [selectedIndex, setSelectedIndex] = useState(0);
   const [selectedItem, setSelectedItem] = useState(items[0]);
 
-  const changeSelectedItem = () => {
-    selectNewItem(selectedIndex, items);
+  const changeSelectedItem = (event:any) => {
+      if(items.length === 1) {
+        event.target.playVideo()
+      } else {
+        selectNewItem(selectedIndex, items);
+      }
   };
 
   const selectNewItem = (index: number, items: string[]) => {
     const condition = index < items.length - 1;
-    const newIndex = condition ? index + 1 : 0;
+    const newIndex = condition ? index + 1 : 0
 
     setSelectedIndex(newIndex);
     setSelectedItem(items[newIndex]);
