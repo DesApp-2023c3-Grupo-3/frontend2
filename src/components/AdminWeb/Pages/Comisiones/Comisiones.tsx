16bc9109d78bb5685cf624112ca03a7372e5ea47
Enzo <enzoalarcon01@gmail.com>
2023-12-05 12:53:12 -0300

    Se agrega opcion eliminar comision en desktop

--- src/components/AdminWeb/Pages/Comisiones/Comisiones.tsx
+++ src/components/AdminWeb/Pages/Comisiones/Comisiones.tsx
@@ -12,6 +12,8 @@ import React from 'react';
 import { MobileBody } from '../../components/Mobile/MobileBody';
 import { FormMobile } from './components/Mobile/FormMobile';
 import { useIsMobile } from '../../hooks/useIsMobile';
+import { Toast } from '../Avisos/components/Form/FormAdvertising';
+import Swal from 'sweetalert2';
 
 function Comisiones() {
   const [commissionsJSON, setCommissionsJSON] = useState<any[]>([]);
@@ -60,6 +62,12 @@ function Comisiones() {
           commission.schedule.endHour,
         ).format('hh:mm')}`,
     ],
+    [
+      '',
+      (commission: Commission) => {
+        return trash(commission.id);
+      },
+    ],
   ]);
 
   const tableColumnsMobile = new Map<string, (advertising: any) => void>([
@@ -84,6 +92,52 @@ function Comisiones() {
   ]);
 
   const isMobile = useIsMobile();
+
+  const handleClickDelete = (id: number) => {
+    Swal.fire({
+      title: '¿Estás seguro?',
+      text: 'No se podrá recuperar la comision.',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, borrar.',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        commissionApi
+          .delete(id)
+          .then(() => {
+            Toast.fire({
+              icon: 'success',
+              title: 'Se ha eliminado la comision',
+            });
+            updateCommissionsTable();
+          })
+          .then(() => updateCommissionsTable())
+          .catch((error) => console.error(error));
+      }
+    });
+  };
+
+  const trash = (id: number) => {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="-3 -2 24 24"
+        className=" cursor-pointer"
+        onClick={() => {
+          handleClickDelete(id);
+        }}
+      >
+        <path
+          fill="currentColor"
+          d="M6 2V1a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v1h4a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-.133l-.68 10.2a3 3 0 0 1-2.993 2.8H5.826a3 3 0 0 1-2.993-2.796L2.137 7H2a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h4zm10 2H2v1h14V4zM4.141 7l.687 10.068a1 1 0 0 0 .998.932h6.368a1 1 0 0 0 .998-.934L13.862 7h-9.72zM7 8a1 1 0 0 1 1 1v7a1 1 0 0 1-2 0V9a1 1 0 0 1 1-1zm4 0a1 1 0 0 1 1 1v7a1 1 0 0 1-2 0V9a1 1 0 0 1 1-1z"
+        />
+      </svg>
+    );
+  };
 
   return (
     <>
